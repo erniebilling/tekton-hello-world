e3602c94b4ff64672fec6e0be321c38a31061900
Ernie Billing <ebilling@yahoo.com>
2026-03-25 18:26:41 -0700

    take 7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+\const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 http.createServer((req, res) => {
   console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from k3s and tekton - take 6!\n');
+  res.end('Hello from k3s and tekton - take 7!\n');
 }).listen(PORT, HOST, () => {
   console.log(`Listening on ${HOST}:${PORT}`);
 });
