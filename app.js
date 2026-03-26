be6e2cda0468bad4ffbf4f211c4e66f421606ac1
Ernie Billing <ebilling@yahoo.com>
2026-03-25 18:59:27 -0700

    try 10

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 http.createServer((req, res) => {
   console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from k3s and tekton - take 9!\n');
+  res.end('Hello from k3s and tekton - take 10!\n');
 }).listen(PORT, HOST, () => {
   console.log(`Listening on ${HOST}:${PORT}`);
 });
